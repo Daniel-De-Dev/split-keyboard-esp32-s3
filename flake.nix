6d35a0017b2cf28f73e04ef6698b279fdc79d1b2
Daniel-De-Dev <154839751+Daniel-De-Dev@users.noreply.github.com>
2025-07-02 16:35:34 +0200

    Set enviroemnt to stable and better tooling

--- flake.nix
+++ flake.nix
@@ -2,29 +2,34 @@
   description = "ESP dev-shell";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     esp-dev.url = "github:mirrexagon/nixpkgs-esp-dev";
     esp-dev.inputs.nixpkgs.follows = "nixpkgs";
   };
-
   outputs = { nixpkgs, esp-dev, ... }:
     let
       system = "x86_64-linux";
       esp-overlay = import "${esp-dev}/overlay.nix";
-
       pkgs = import nixpkgs {
         inherit system;
         overlays = [ esp-overlay ];
       };
+
+      idf = esp-dev.packages.${system}.esp-idf-esp32s3.override (final: {
+        toolsToInclude = final.toolsToInclude ++ [
+          "esp-clang"
+        ];
+      });
     in
     {
       devShells.${system}.default = pkgs.mkShell {
         name = "esp32-s3-project";
-
         buildInputs = with pkgs; [
-          esp-idf-esp32s3
-          clang-tools
+          idf
         ];
+        shellHook = ''
+          export CLANGD_QUERY_DRIVER=`which xtensa-esp32-elf-g++`
+        '';
       };
     };
 }
